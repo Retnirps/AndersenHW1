1498a3a6015247a6488f3a3b856736b731868fb3
Daniel <Vasiliy23898@gmail.com>
2021-09-28 14:06:18 +0300

    second commit

--- app/src/main/java/com/majestadev/myapplication/MainActivity.kt
+++ app/src/main/java/com/majestadev/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.majestadev.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Toast.makeText(this, "text", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.activity_main)
     }
 }
